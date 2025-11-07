bf25e7c6c1796dd0700ed3e891ea81e4cb0472be
Shraddha <shraddhabodke99@gmail.com>
2025-11-07 15:58:41 +0530

    Version1

---
+++ Test.py
@@ -0,0 +1,48 @@
+# Addition of 2 integers
+
+a = 19
+b = 39
+print("Addition is :" , a+b)
+
+#Arithmetic operations of Floating point numbers
+
+c = 86.46
+d = 14.23
+print("Addition is :" , c+d)
+print(" Subtraction is :" , c-d)
+print("Multiplication is :" , c*d)
+print("Division is :" , c//d)
+
+#Length of a string
+
+e = "My husband is very supportive"
+print("Length os the string is :", len(e))
+
+#Converting string into Upper and Lower case
+
+free = "DevSecOps is the future of the SD"
+g = free.upper()
+h = free.lower()
+print(g)
+print(h)
+
+# Replacing substring within a string
+
+ice = "Don't follow others, create your own identity"
+j = ice.replace("Don't" , "Do")
+print(j)
+
+
+#Split the text of string
+k=ice.split( )
+print(k)
+
+#Define a variable and assign veriables
+
+integers = [10, 20, 30, 40, 50]
+print(integers)
+
+#Define a list of variables using tuple
+
+variable_t = (10, 20, 40, 10, 30)
+print(variable_t)
